c3d87cab560483dc60beb6677f760367f3a81904
Jorge Rodriguez <foxpower@gmail.com>
2016-11-05 11:36:14 -0500

    Desarrollo_De_Aplicaciones_iOS
    1. First Commit with basic stuff using playground

---
+++ Swift-iOS/Week1/Hola.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var nombre = " Jorge"
+
+let resultado : String = str + nombre
+
+print(nombre)
+
+print( "Esto es demo con Swift " + resultado)
+
+// Emojic
+print("Lista de deportes: \n\t1. Baloncesto \n\t2. Futbol \n\t3. 🏊🏽")
